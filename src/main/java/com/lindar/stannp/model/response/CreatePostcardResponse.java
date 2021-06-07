6f587ea9ce1b3cb959252dec7b31ff7a353a31f3
Steven Hills <steven.hills@gmail.com>
2021-06-07 14:13:28 +0100

    improvement: include all details in the create responses

--- src/main/java/com/lindar/stannp/model/response/CreatePostcardResponse.java
+++ src/main/java/com/lindar/stannp/model/response/CreatePostcardResponse.java
@@ -2,7 +2,15 @@ package com.lindar.stannp.model.response;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+
 @Data
 public class CreatePostcardResponse {
     private String pdf;
+    private String id;
+    private Instant created;
+    private String format;
+    private BigDecimal cost;
+    private String status;
 }
